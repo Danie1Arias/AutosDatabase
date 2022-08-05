71970cf55803dba898c61988f1a019da4815e239
Daniel Arias <daniel.arias@estudiants.urv.cat>
2022-08-05 13:20:12 +0200

    Finished

--- index.php
+++ index.php
@@ -1,25 +1,21 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Charles Severance - Broken Rock Paper Scissors fbe6adc9</title>
+<title>Welcome to Autos Database 73292a3f</title>
 <?php require_once "bootstrap.php"; ?>
 </head>
 <body>
 <div class="container">
-<h1>Welcome to Broken Rock Paper Scissors</h1>
-<p><strong>Note:</strong> This sample code is only
-partially done and serves only as a starting point for the assignment.
-</p>
+<h1>Welcome to Autos Database fbe6adc9</h1>
 <p>
 <a href="login.php">Please Log In</a>
 </p>
 <p>
 Attempt to go to 
-<a href="game.php">game.php</a> without logging in - it should fail with an error message.
+<a href="autos.php">autos.php</a> without logging in - it should fail with an error message.
 <p>
-<a href="http://www.wa4e.com/code/rps.zip"
+<a href="https://www.wa4e.com/code/pdo.zip"
  target="_blank">Source Code for this Application</a>
 </p>
 </div>
 </body>
-
